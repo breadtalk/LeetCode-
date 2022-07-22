104f06369d32ac625f6b13c6ec137b7976be8dfb
breadtalk <sukieyan@gmail.com>
2022-07-21 23:16:32 -0700

    Update 144. Preorder Traversal.java

--- Binary Tree/144. Preorder Traversal.java
+++ Binary Tree/144. Preorder Traversal.java
@@ -1,3 +1,5 @@
+第一个用recursive method 第二个用stack
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -27,5 +29,47 @@ class Solution {
         returnList.add(current.val);
         preorderTraversalR(current.left, returnList);
         preorderTraversalR(current.right, returnList);
+    }
+}
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> returnList = new LinkedList<>();
+        if(root == null){
+            return returnList;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode curr = stack.pop();
+            returnList.add(curr.val);
+            if(curr.right!=null){
+                stack.push(curr.right);
+            }
+            if(curr.left!=null){
+                stack.push(curr.left);
+            }      
+        }
+        return returnList;
     }
 }
