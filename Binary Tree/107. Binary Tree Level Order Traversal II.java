1904af38e96c5d40027809226340a2eee3cdd2ef
breadtalk <sukieyan@gmail.com>
2022-07-25 23:14:54 -0700

    Update 107. Binary Tree Level Order Traversal II.java

--- Binary Tree/107. Binary Tree Level Order Traversal II.java
+++ Binary Tree/107. Binary Tree Level Order Traversal II.java
@@ -1,4 +1,6 @@
 和102题很类似用DFS来做level order traversal
+第二个方法用BFS来做
+
 
 /**
  * Definition for a binary tree node.
@@ -36,5 +38,60 @@ class Solution {
         levelOrderDFS(returnList, node.left, depth);
         levelOrderDFS(returnList, node.right, depth);
     }
+}
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> returnList = new LinkedList<List<Integer>>();
+        if(root == null){
+            return returnList;
+        }
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        levelOrderBFS(queue, returnList);
+        return returnList;
+    }
+    public void levelOrderBFS(ArrayDeque<TreeNode> queue, LinkedList<List<Integer>> returnList){
+        if(queue.size() == 0){
+            return;
+        }
+        List<Integer> sublist = new LinkedList<>();
+        int len = queue.size();
+        TreeNode curr;
+        for(int i=0; i<len; i++){
+            curr = queue.poll();
+            sublist.add(curr.val);
+            if(curr.left != null){
+                queue.add(curr.left);
+            }
+            if(curr.right != null){
+                queue.add(curr.right);
+            }
+        }
+        returnList.addFirst(sublist);
+        levelOrderBFS(queue,returnList);
+    }
+    
+    
 }
 
