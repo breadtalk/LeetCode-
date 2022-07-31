34f5cecb050c20b969b25a6cc68d1d007d30413a
breadtalk <sukieyan@gmail.com>
2022-07-30 23:09:17 -0700

    Update 226. Invert Binary Tree.java

--- Binary Tree/226. Invert Binary Tree.java
+++ Binary Tree/226. Invert Binary Tree.java
@@ -1,5 +1,6 @@
 第一种办法用level order traversal BFS
 第二种办法用的preorder traversal 
+第三种办法用preorder traversal stack
 
 /**
  * Definition for a binary tree node.
@@ -91,6 +92,52 @@ class Solution {
         }
     }
     
+    public void switchNode(TreeNode current){
+        TreeNode temp = current.left;
+        current.left = current.right;
+        current.right = temp;
+    }
+}
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode current = stack.pop();
+            switchNode(current);
+            if(current.right!=null){
+                stack.push(current.right);
+            }
+            if(current.left!=null){
+                stack.push(current.left);
+            }
+        }
+        return root;
+    }
     public void switchNode(TreeNode current){
         TreeNode temp = current.left;
         current.left = current.right;
