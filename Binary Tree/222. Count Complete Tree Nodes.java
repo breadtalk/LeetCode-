fdd66c8095d1ca6869021008e0767f4944b6ab48
breadtalk <sukieyan@gmail.com>
2022-08-08 22:28:57 -0700

    Update 222. Count Complete Tree Nodes.java

--- Binary Tree/222. Count Complete Tree Nodes.java
+++ Binary Tree/222. Count Complete Tree Nodes.java
@@ -1,5 +1,6 @@
-第一个方法用普通recursive preorder 
-
+计算左边树和右边树的高度
+如果左边树和右边树一样高那左边是完整树 return 1 + 左边完整树节点加上右边count
+如果右边树矮 拿右边是完整树 return 1 + 右边完整树节点 + 左边count
 
 
 /**
@@ -18,21 +19,27 @@
  * }
  */
 class Solution {
-    int count = 0;
     public int countNodes(TreeNode root) {
         if(root == null){
             return 0;
         }
-        countNodesPreOrder(root);
-        return count;
+        int left = countDepth(root.left);
+        int right = countDepth(root.right);
+        if(left == right){
+            return 1 + ((int)Math.pow(2,left)-1) + countNodes(root.right);
+        }
+        else{
+            return 1 + ((int)Math.pow(2, right)-1) + countNodes(root.left);
+        }
         
     }
-    public void countNodesPreOrder(TreeNode node){
-        if(node == null){
-            return;
+    
+    public int countDepth(TreeNode node){
+        int depth = 0;
+        while (node != null){
+            depth ++;
+            node = node.left;
         }
-        count++;
-        countNodesPreOrder(node.left);
-        countNodesPreOrder(node.right);
+        return depth;
     }
 }
