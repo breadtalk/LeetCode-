25ec89b8749c91feee57bf7ee0a3615c74fb6b38
breadtalk <sukieyan@gmail.com>
2022-08-16 14:25:46 -0700

    Create Star 77. Combinations.java

---
+++ BackTracking/Star 77. Combinations.java
@@ -0,0 +1,23 @@
+用模板
+
+class Solution {
+    
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> subList = new LinkedList<>();
+    
+    public List<List<Integer>> combine(int n, int k) {
+        combineHelper(n, k, 1);
+        return result;
+    }
+    public void combineHelper(int n, int k, int startPoint){
+        if(subList.size() == k){
+            result.add(new LinkedList(subList)); //这里一定要新建一个list 不然的话return的时候就全return完了 变成空子集了
+            return; // 一定要记住return
+        }
+        for(int i = startPoint; i <= n; i++){
+            subList.add(i);
+            combineHelper(n, k, i+1);
+            subList.removeLast(); //backtracking step here 
+        }
+    }
+}
