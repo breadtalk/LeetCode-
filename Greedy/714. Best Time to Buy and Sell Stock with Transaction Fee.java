e102ce2f4272424c654d6da1ca5861640c74fcb7
breadtalk <sukieyan@gmail.com>
2022-08-26 16:36:23 -0700

    Update 714. Best Time to Buy and Sell Stock with Transaction Fee.java

--- Greedy/714. Best Time to Buy and Sell Stock with Transaction Fee.java
+++ Greedy/714. Best Time to Buy and Sell Stock with Transaction Fee.java
@@ -11,7 +11,10 @@ class Solution {
                 profit = profit + prices[i] - buyPrice - fee;
                 buyPrice = prices[i] - fee;                              //最关键的一步 重新update buyPrice 保证不重复计算profit并且不重复减fee
             }
-            else{                                                        //当前price大于buyprice 但是并没有赚钱 跳过 等待要么更低价钱买入 要么等涨上去收profit
+            else{                                                        //当前price大于buyprice 但是并没有赚钱 跳过 等待要么更低价钱买入 要么等涨上去收profit从代码中
+                                                                          //可以看出对情况一的操作，因为如果还在收获利润的区间里，表示并不是真正的卖出，
+                                                                          //而计算利润每次都要减去手续费，所以要让minPrice = prices[i] - fee;，
+                                                                          //这样在明天收获利润的时候，才不会多减一次手续费！
                 continue;
             }
         }
