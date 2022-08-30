212900ba149371f12558132a90232b2edbddccae
breadtalk <sukieyan@gmail.com>
2022-08-29 22:35:36 -0700

    Create 343. Integer Break.java

---
+++ DP/343. Integer Break.java
@@ -0,0 +1,32 @@
+第一个办法找规律写出来的
+class Solution {
+    public int integerBreak(int n) {
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+        int result = 1;
+        int count = n / 3;
+        int residue = n % 3;
+        if(residue == 0){
+            for(int i = count; i > 0; i--){
+                result = result * 3;
+            }
+        }
+        else if(residue == 2){
+            for( int i = count; i > 0; i--){
+                result = result * 3;
+            }
+            result =result * residue;
+        }
+        else{
+            for(int i = count; i > 1; i--){
+                result = result *3;
+            }
+            result = result * 4;
+        }
+        return result;
+    }
+}
