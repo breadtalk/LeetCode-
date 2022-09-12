d454f5950009053bfd4f208f7dc90e95444ef37d
breadtalk <sukieyan@gmail.com>
2022-09-12 16:01:15 -0700

    Create 1035. Uncrossed Lines.java

---
+++ DP/子序列/1035. Uncrossed Lines.java
@@ -0,0 +1,22 @@
+本质和1143一样求最长公共子序列
+
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int result = 0;
+        for(int i = 1; i <= nums1.length; i++){
+            for(int j = 1; j <= nums2.length; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+                if(result < dp[i][j]){
+                    result = dp[i][j];
+                }
+            }
+        }
+        return result;
+    }
+}
